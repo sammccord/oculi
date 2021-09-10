2e3b06121624aeaf2c36432314f55128d1624401
king esports mandingo inches <sam.mccord@protonmail.com>
2021-09-10 13:19:00 -0600

    release v0.0.0-canary

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as eta from "eta";
-import get from 'lodash.get'
-import set from 'lodash.set'
+import get from "lodash.get";
+import set from "lodash.set";
 import type { EtaConfig } from "eta/dist/types/config";
 import type { TemplateFunction } from "eta/dist/types/compile";
 
@@ -8,11 +8,11 @@ export class Oculi<Data> {
   public static Eta = eta;
   public static rng = Math.random;
 
-  protected config: EtaConfig
+  protected config: EtaConfig;
   protected compiled: TemplateFunction;
   protected _data: any = {
     sample: this.sample.bind(this),
-    memo: this.memo.bind(this)
+    memo: this.memo.bind(this),
   };
 
   constructor(
@@ -20,32 +20,33 @@ export class Oculi<Data> {
     data: Partial<Data> = {},
     config?: Partial<EtaConfig>
   ) {
-    this.config = { ...eta.config, config }
+    this.config = { ...eta.config, config };
     this.compiled = eta.compile(template, this.config);
     for (let k in data) {
-      this._data[k] = data[k]
+      this._data[k] = data[k];
     }
   }
 
   public get data(): Data {
-    return { ...this.data }
+    return { ...this.data };
   }
 
   protected memo<T>(key: string, value?: T): T {
-    let val = get(this._data, key)
-    if(!val) {
-      val = value
-      set(this._data, key, value)
+    let val = get(this._data, key);
+    if (!val) {
+      val = value;
+      set(this._data, key, value);
     }
-    return val
+    return val;
   }
 
   protected sample(val: string | any[]): any {
-    if(typeof val === 'string' && Array.isArray(this._data[val])) return this.sample(this._data[val])
+    if (typeof val === "string" && Array.isArray(this._data[val]))
+      return this.sample(this._data[val]);
     return val[Math.floor(Oculi.rng() * val.length)];
   }
 
   public generate(config?: EtaConfig) {
-    return this.compiled(this._data, config || this.config)
+    return this.compiled(this._data, config || this.config);
   }
 }
