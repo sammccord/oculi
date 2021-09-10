265fea6242ed1d837538549e3e91d8ebbb44df44
Sam McCord <sam.mccord@protonmail.com>
2021-09-10 12:36:11 -0600

    tests working

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,41 @@
-import { Freta } from "./index";
+import { Oculi } from "./index";
 
-test("", () => {
-  const t = Freta.generate();
-  console.log(t);
-  expect(typeof t).toBe("string");
+it("Basic templating works", () => {
+  const oculi = new Oculi(`His name is <%= it.name %>.`, {
+    name: "Robert Paulson",
+  });
+  expect(oculi.generate()).toBe(`His name is Robert Paulson.`);
+});
+
+it("Memoization works, later memo calls to the same key will return the first memo'd value", () => {
+  const oculi = new Oculi(
+    `His name is <%= it.memo('hisName', 'Robert Paulson') %>. His name is <%= it.memo('hisName', 'Some other idiot') %>.`
+  );
+  expect(oculi.generate()).toBe(
+    `His name is Robert Paulson. His name is Robert Paulson.`
+  );
+});
+
+describe("Sampling", () => {
+  beforeAll(() => {
+    let i = 0;
+    Oculi.rng = () => {
+      i += 0.33;
+      return i;
+    };
+  });
+
+  it("works with custom seeds", () => {
+    const oculi = new Oculi(
+      `His name is <%= it.sample('names') %>. <%= it.sample('names') %> turned around and left.`,
+      { names: ["Robert Paulson", "Tyler Durden"] }
+    );
+    expect(oculi.generate()).toBe(
+      `His name is Robert Paulson. Tyler Durden turned around and left.`
+    );
+  });
+
+  afterAll(() => {
+    Oculi.rng = Math.random;
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,51 @@
 import * as eta from "eta";
+import get from 'lodash.get'
+import set from 'lodash.set'
 import type { EtaConfig } from "eta/dist/types/config";
 import type { TemplateFunction } from "eta/dist/types/compile";
 
-export class Oculi {
-  public static _eta = eta;
+export class Oculi<Data> {
+  public static Eta = eta;
+  public static rng = Math.random;
+
+  protected config: EtaConfig
   protected compiled: TemplateFunction;
-  protected memod: object = {};
-  protected data: object = {
-    memo: this.memo,
+  protected _data: any = {
+    sample: this.sample.bind(this),
+    memo: this.memo.bind(this)
   };
 
-  constructor(template: string, data: object, options: Partial<EtaConfig>) {
-    this.compiled = eta.compile(template, options);
+  constructor(
+    template: string,
+    data: Partial<Data> = {},
+    config?: Partial<EtaConfig>
+  ) {
+    this.config = { ...eta.config, config }
+    this.compiled = eta.compile(template, this.config);
     for (let k in data) {
+      this._data[k] = data[k]
     }
   }
 
-  static generate() {
-    return eta.render("The answer to everything is <%= it.t %>", {
-      get [t]() {
-        return "cool";
-      },
-    });
+  public get data(): Data {
+    return { ...this.data }
   }
 
-  protected memo(grammar: string, key?: string) {}
+  protected memo<T>(key: string, value?: T): T {
+    let val = get(this._data, key)
+    if(!val) {
+      val = value
+      set(this._data, key, value)
+    }
+    return val
+  }
+
+  protected sample(val: string | any[]): any {
+    if(typeof val === 'string' && Array.isArray(this._data[val])) return this.sample(this._data[val])
+    return val[Math.floor(Oculi.rng() * val.length)];
+  }
+
+  public generate(config?: EtaConfig) {
+    return this.compiled(this._data, config || this.config)
+  }
 }
